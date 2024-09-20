7b5b0cdf14d68a979cb67996b8df8ce50defdde0
Kumar <kumar.1992703@studenti.uniroma1.it>
2024-09-20 05:15:37 +0200

    ciou

--- losses.py
+++ losses.py
@@ -195,10 +195,11 @@ class SetCriterion(nn.Module):
         src_boxes = outputs['pred_boxes'][idx]
         target_boxes = torch.cat([t['boxes'][i] for t, (_, i) in zip(targets, indices)], dim=0)
 
-        loss_bbox = F.l1_loss(src_boxes, target_boxes, reduction='none')
+        
 
         losses = {}
-        losses['loss_bbox'] = loss_bbox.sum() / num_boxes
+        
+
         
 
 
@@ -206,14 +207,17 @@ class SetCriterion(nn.Module):
             loss_ciou = 1 - torch.diag(box_ops.ciou(
                     box_ops.box_cxcywh_to_xyxy(src_boxes),
                     box_ops.box_cxcywh_to_xyxy(target_boxes)))
-            losses['loss_ciou'] = loss_ciou.sum() / num_boxes
-            
+            losses['loss_bbox'] = loss_ciou.sum() / num_boxes
         else:
-            loss_giou = 1 - torch.diag(box_ops.generalized_box_iou(
-                    box_ops.box_cxcywh_to_xyxy(src_boxes),
-                    box_ops.box_cxcywh_to_xyxy(target_boxes)))
-            losses['loss_giou'] = loss_giou.sum() / num_boxes
-            losses['loss_ciou'] = 0
+            loss_bbox = F.l1_loss(src_boxes, target_boxes, reduction='none')
+            losses['loss_bbox'] = loss_bbox.sum() / num_boxes
+            
+            
+        loss_giou = 1 - torch.diag(box_ops.generalized_box_iou(
+                box_ops.box_cxcywh_to_xyxy(src_boxes),
+                box_ops.box_cxcywh_to_xyxy(target_boxes)))
+        losses['loss_ciou'] = loss_giou.sum() / num_boxes
+            
             
             
         return losses
