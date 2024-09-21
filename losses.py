f6e8c01176cd64351554f890effd7a20d3bc1656
Kumar <kumar.1992703@studenti.uniroma1.it>
2024-09-21 05:19:49 +0200

    different labels

--- losses.py
+++ losses.py
@@ -202,8 +202,8 @@ class SetCriterion(nn.Module):
         
 
         losses = {}
-        #loss_bbox = F.l1_loss(src_boxes, target_boxes, reduction='none')
-        #losses['loss_bbox'] = loss_bbox.sum() / num_boxes
+        loss_bbox = F.l1_loss(src_boxes, target_boxes, reduction='none')
+        losses['loss_bbox'] = loss_bbox.sum() / num_boxes
         
 
         
@@ -214,7 +214,7 @@ class SetCriterion(nn.Module):
                     box_ops.box_cxcywh_to_xyxy(src_boxes),
                     box_ops.box_cxcywh_to_xyxy(target_boxes)))
             losses['loss_giou'] = loss_ciou.sum() / num_boxes
-            losses['loss_bbox'] = loss_ciou.sum() / num_boxes
+           #losses['loss_bbox'] = loss_ciou.sum() / num_boxes
         else:
 
           loss_giou = 1 - torch.diag(box_ops.generalized_box_iou(
