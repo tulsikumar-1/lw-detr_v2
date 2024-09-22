01304227f23c533de4e316fc849def01248c6430
Kumar <kumar.1992703@studenti.uniroma1.it>
2024-09-22 22:57:19 +0200

    dataset scales

--- build_dataset.py
+++ build_dataset.py
@@ -139,7 +139,7 @@ def make_coco_transforms_square_div_64(image_set):
     ])
 
 
-    scales = [ 512, 576, 640, 704, 768, 832, 896]
+    scales = [448,512, 576, 640, 704, 768, 832]
 
     if image_set == 'train':
         return T.Compose([
