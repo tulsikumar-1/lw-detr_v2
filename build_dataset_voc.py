573429b5478d85e911b54699e6e246c2ede900a6
Kumar <kumar.1992703@studenti.uniroma1.it>
2024-09-23 10:00:15 +0200

    dataset scales

--- build_dataset_voc.py
+++ build_dataset_voc.py
@@ -145,7 +145,12 @@ def make_coco_transforms_square_div_64(image_set):
         return T.Compose([
             T.RandomHorizontalFlip(),
             T.RandomSelect(
-                T.SquareResize(scales),T.RandomSizeCrop(300, 450),
+                T.SquareResize(scales),
+                T.Compose([
+                    #T.RandomResize([400, 500, 600]),
+                    T.RandomSizeCrop(384, 600),
+                    T.SquareResize(scales),
+                ]),
             ),
             normalize,
         ])
